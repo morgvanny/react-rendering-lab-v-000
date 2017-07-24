97705eede1919a9a7d0c2eb236fad28328fb7207
morgvanny <morgvanny@gmail.com>
2017-07-24 12:23:26 -0600

    Done.

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -10,6 +10,13 @@ class Rating extends React.Component {
       decreasing: false
     };
   }
+  
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      increasing: this.props.rating < nextProps.rating,
+      decreasing: this.props.rating > nextProps.rating
+    })
+  }
 
   render() {
     let trend = 'stable';
